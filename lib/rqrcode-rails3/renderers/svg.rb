102807579c5cab8318db5b66390f2cfa63278c94
Alejandro Andrés <alej@redradix.com>
2014-05-26 12:39:59 +0200

    Update svg.rb

--- lib/rqrcode-rails3/renderers/svg.rb
+++ lib/rqrcode-rails3/renderers/svg.rb
@@ -18,7 +18,7 @@ module RQRCode
           dimension = (qrcode.module_count*unit) + (2*offset)
 
           xml_tag   = %{<?xml version="1.0" standalone="yes"?>}
-          open_tag  = %{<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ev="http://www.w3.org/2001/xml-events" width="304" height="304">}
+          open_tag  = %{<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ev="http://www.w3.org/2001/xml-events" width="304" height="304" viewBox="0 0 330 330">}
           close_tag = "</svg>"
 
           result = []
